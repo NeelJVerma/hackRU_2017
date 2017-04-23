d7a5774fa8c8a22d0b79ef8ca73ffeefd5a9760f
josh long <jlong2@ramapo.edu>
2017-04-22 23:03:55 -0400

    changes to _init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -41,29 +41,33 @@ def messages():
 			'zip_code':request.form['FromCity'],
 			'country':request.form['FromCountry']
 		}
-	location_scrap('starbucks',request.form['FromCity'])
+	data = location_scrap('starbucks',request.form['FromCity'])
+	
 	mongo.db.askHer.insert_one(user_info)
 	return str(resp)
 
 
 def location_scrap(business_name,loc):
-	#establishes connection to google's servers based on a valid auth key
-	google_places = GooglePlaces(app.config['GOOGLE_KEY']) 
+        #establishes connection to google's servers based on a valid auth key
+        google_places = GooglePlaces('AIzaSyCC_weuF9RVrynjabaplRUgjpZyHuD2vuM')
 
-	# You may prefer to use the text_search API, instead.
-	query_result = google_places.nearby_search(location=loc, keyword=business_name,radius=20000, types=[types.TYPE_FOOD])
-	# If types param contains only 1 item the request to Google Places API
-	# will be send as type param to fullfil:
-	# http://googlegeodevelopers.blogspot.com.au/2016/02/changes-and-quality-improvements-in_16.html
+        # You may prefer to use the text_search API, instead.
+        query_result = google_places.nearby_search(location=loc, keyword=business_name,radius=20000, types=[types.TYPE_FOOD])
+        # If types param contains only 1 item the request to Google Places API
+        # will be send as type param to fullfil:
+        # http://googlegeodevelopers.blogspot.com.au/2016/02/changes-and-quality-improvements-in_16.html
 
-	if query_result.has_attributions:
-			print(query_result.html_attributions)
+        if query_result.has_attributions:
+                        print(query_result.html_attributions)
+        data = []
 
-	for place in query_result.places:
-			# Returned places from a query are place summaries.
-		print(place.name)
-		print(place.rating)
-		place.get_details()
-		print(place.details['reviews'])
-		#for review in place.details['reviews']:
-			#print(review["text"])
+        for place in query_result.places:
+                place.get_details()
+                # Returned places from a query are place summaries.
+                reviews = []
+                for review in place.details['reviews']:
+                        review_data = {"text":review["text"],"rating":review["rating"],"time":review["time"]}
+                        reviews.append(review_data)
+                place_data = (place.name,place.rating,reviews)
+                data.append(place_data)
+        return data
